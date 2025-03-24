c98dde693373688693edeb0e5c4b5f778e8fca90
JoseJCanedo <canedo_jose@columbusstate.edu>
2025-03-24 16:27:45 +0000

    render add

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
 const path = require('path');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ encoded: false }))
 
-app.get('/', function(req, res){
-    // req.query.userName || 'CPSC'     A or B
-    // const foo = req.query.userName ? req.query.userName : 'CPSC'
-    // const bar = req.boolean && 'CPSC'
-    res.render('index', {userName: req.query.userName || 'CPSC'});
+var tasks = ["create to do list", "add items"]
+
+app.get('/', function (req, res) {
+    res.render('index', { tasks: tasks || [] });
 })
 
-app.get('/name/:userName', function(req, res){
-    res.render('index', {userName: req.params.userName});
+app.post('/add', function (req, res) {
+    tasks.push(req.body.newTask)
+    res.redirect('/');
 })
 
-app.listen(3000, function(){
+app.post('/done', function (req, res) {
+    if (typeof req.body.task !== 'string') {
+        tasks = tasks.filter(item => item !== req.body.task)
+    } else if (Array.isArray(req.body.task)) {
+        tasks = tasks.filter(item => req.body.task.indexOf(item) === -1)
+    } else {
+        console.warn(`Data type is not correct receieved type ${typeof req.body.task}. Please check input args: `, req.body.task)
+    }
+    res.redirect('/');
+})
+
+app.listen(3000, function () {
     console.log('Our app is running on port 3000');
 })
